3ffd41dbf5dcae36bb23a74c4e5c442c9d054bb1
Raga0074 <mharshal517@gmail.com>
2024-11-08 00:12:44 +0530

    in progress

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+
 
 function Header() {
   return (
     <>
-    <div className='flex items-center p-2'>
-        <div className='text-lg button'>
+    <div className='flex items-center p-2 justify-between bg-orange-500'>
+      <h6 className='text-black text-xs sm:text-lg sm:flex-shrink-0'>"Elevate Your Look, Define Your Essence – Wear What Speaks to You!!!"</h6>
+        <div className=''>
           <SignedOut>
             <SignInButton />
           </SignedOut>
@@ -18,7 +20,7 @@ function Header() {
           <div className='flex justify-between align-middle'>
             <img src='/kase.svg' width={90}/>
             <div className='flex gap-3'>
-              <img className='w-8 cursor-pointer' src='/cart.svg'/>
+              <img className='cart w-8 cursor-pointer' src='/cart.svg'/>
               <img className='w-8 cursor-pointer' src='/favorites.svg'/>
             </div>
           </div>
